624a41bd0893e22f0223b2750bb39b43383f8ca3
Kajanan02 <Kajanan02000@gmail.com>
2024-06-25 01:01:48 +0530

    first commit

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:8800/api-v1";
+const API_URL = "https://job-portal-final.onrender.com/api-v1";
 
 export const API = axios.create({
   baseURL: API_URL,
